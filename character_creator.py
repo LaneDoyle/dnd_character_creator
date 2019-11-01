55a555f1f9a13384996bc158ff1e4a7351c64fe4
LaneDoyle <ldoyle@stcharlessd.org>
2019-11-01 13:34:10 -0500

    modified:   character_creator.py

--- character_creator.py
+++ character_creator.py
@@ -5,7 +5,7 @@
 ''' A program which helps a beginner learn how to create D&D characters'''
 
 #Variables
-user_command = 0 #Determines which command user has typed in and acts accordingly
+user_command = "" #Determines which command user has typed in and acts accordingly
 
 
 
@@ -17,8 +17,8 @@ user_command = 0 #Determines which command user has typed in and acts accordingl
 #Introduction
 print('''Welcome to the D&D Character Creator! This program can help beginners
 make their first D&D character at first level!''')
-user_command = input("Confused on where to start? Type HELP. Or, type in the command you wish you use. ")
-if user_command == "CLASSES":
+user_command = input("Confused on where to start? Type HELP. Or, type in the command you wish you use. (Type PLAY to start!) ")
+if user_command == "CLASSES": 
     print(''' Available classes:''')
     user_command = input("Please type in the command you wish you use. ")
     if user_command == "DIE":
@@ -107,7 +107,8 @@ if user_command == "HELP":
     CLASSES: shows the list of classes available to the player.
     DIE: shows the types of die available for use.
     RACES: shows the available races and their attributes.
-    BACK: returns to the start screen.''')
+    BACK: returns to the start screen.
+    PLAY: begins the creation process''')
     user_command = input("Please type in the command you wish you use. ")
     if user_command == "CLASSES":
         print(''' Available classes:''')
@@ -182,7 +183,7 @@ elif user_command == "RACES":
         1d6- a six sided dice using numbers 1-6.
         1d8- an eight sided dice using numbers 1-8.
         1d12- a twelve sided dice using numbers 1-12.
-        1d20- a twenty sided dice using numbers 1-20.''') 
+        1d20- a twenty sided dice using numbers 1-20.''')
 #Back Menu
 elif user_command == "BACK":
     print('''Welcome to the D&D Character Creator! This program can help beginners
@@ -208,6 +209,60 @@ elif user_command == "BACK":
         1d6- a six sided dice using numbers 1-6.
         1d8- an eight sided dice using numbers 1-8.
         1d12- a twelve sided dice using numbers 1-12.
-        1d20- a twenty sided dice using numbers 1-20.''')        
+        1d20- a twenty sided dice using numbers 1-20.''') 
+#Play Menu
+elif user_command == "PLAY":
+    #Fake loading
+    print('''
+    
+    
+    
+    Creating Creation Process....
+            
+            
+            
+            
+            
+            
+            
+            
+            
+    Creating Races...
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+    Killing off Goblins...
+    
+    ''')  
+    
+    print('''
+    Alright! Let's start!
+    ''')
+    
+    print(''' 
+    There are several layers to the character creation process. First, let's start with your character's 
+    race. You surely looked at the races beforehand...right? Just kidding! I know you didn't. So, I grabbed 
+    an extra copy of the list of races! Here they are: 
+    ''')
+    
+    print(''' 
+    Barbarian
+    Warlock
+    Fighter
+    Wizard
+    Sorcerer
+    Monk
+    Paladin
+    Cleric
+    ''')
+    
 
 
