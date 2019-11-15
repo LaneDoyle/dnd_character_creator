3fc9a7b4d5f225d2939556474f3b90db8fe05865
LaneDoyle <ldoyle@stcharlessd.org>
2019-11-15 13:40:22 -0600

    modified:   character_creator.py

--- character_creator.py
+++ character_creator.py
@@ -22,7 +22,61 @@ stat_cha = 0 #Charisma Score
 
 
 #Functions
+def Menu_classes():
+        print(''' Available classes:
+        Barbarian
+        Bard
+        Cleric
+        Druid
+        Fighter
+        Monk
+        Paladin
+        Ranger
+        Rogue
+        Sorcerer
+        Warlock
+        Wizard''')
+        user_command = input("Please type in the command you wish you use. ")
 
+def Menu_races():
+        print(''' Available races:
+        Dwarf
+        Elf
+        Halfling
+        Human
+        Dragonborn
+        Gnomes
+        Half-Elf
+        Half-Orc
+        Tiefling''')
+        user_command = input("Please type in the command you wish you use. ")
+
+def Menu_die():
+        print('''Die:
+        1d4- a four sided dice using numbers 1-4.
+        1d6- a six sided dice using numbers 1-6.
+        1d8- an eight sided dice using numbers 1-8.
+        1d12- a twelve sided dice using numbers 1-12.
+        1d20- a twenty sided dice using numbers 1-20.''')
+        user_command = input("Please type in the command you wish you use. ")
+
+def Menu_back():
+        print('''Welcome to the D&D Character Creator! This program can help beginners
+        make their first D&D character at first level!''')
+        user_command = input("Confused on where to start? Type HELP. Or, type in the command you wish you use. ")
+
+def Menu_help():
+        print(''' Commands:
+        HELP: shows the list of commands users can use.
+        CLASSES: shows the list of classes available to the player.
+        DIE: shows the types of die available for use.
+        RACES: shows the available races and their attributes.
+        BACK: returns to the start screen.
+        PLAY: begins the creation process''')
+        user_command = input("Please type in the command you wish you use. ")
+
+    
+    
 
 
 #Code
@@ -34,279 +88,19 @@ backgrounds which appear in the Player's Handbook.''')
 
 user_command = input("Confused on where to start? Type HELP. Or, type in the command you wish you use. (Type PLAY to start!) ")
 if user_command == "CLASSES": 
-    print(''' Available classes:''')
-    user_command = input("Please type in the command you wish you use. ")
-    if user_command == "DIE":
-        print('''Die:
-        1d4- a four sided dice using numbers 1-4.
-        1d6- a six sided dice using numbers 1-6.
-        1d8- an eight sided dice using numbers 1-8.
-        1d12- a twelve sided dice using numbers 1-12.
-        1d20- a twenty sided dice using numbers 1-20.''')
-        user_command = input("Please type in the command you wish you use. ")
-    elif user_command == "RACES":
-        print(''' Available races:
-        Dwarf
-        Elf
-        Halfling
-        Human
-        Dragonborn
-        Gnomes
-        Half-Elf
-        Half-Orc
-        Tiefling''')
-        user_command = input("Please type in the command you wish you use. ")
-    elif user_command == "BACK":
-        print('''Welcome to the D&D Character Creator! This program can help beginners
-        make their first D&D character at first level!''')
-        user_command = input("Confused on where to start? Type HELP. Or, type in the command you wish you use. ")  
-#Die Menu
+        Menu_classes()
+        Menu_back()
 elif user_command == "DIE":
-    print('''Die:
-    1d4- a four sided dice using numbers 1-4.
-    1d6- a six sided dice using numbers 1-6.
-    1d8- an eight sided dice using numbers 1-8.
-    1d12- a twelve sided dice using numbers 1-12.
-    1d20- a twenty sided dice using numbers 1-20.''')
-    user_command = input("Please type in the command you wish you use. ")
-    if user_command == "CLASSES":
-        print(''' Available classes:''')
-        user_command = input("Please type in the command you wish you use. ")        
-    elif user_command == "RACES":
-        print(''' Available races:
-        Dwarf
-        Elf
-        Halfling
-        Human
-        Dragonborn
-        Gnomes
-        Half-Elf
-        Half-Orc
-        Tiefling''')
-        user_command = input("Please type in the command you wish you use. ")
-    elif user_command == "BACK":
-        print('''Welcome to the D&D Character Creator! This program can help beginners
-        make their first D&D character at first level!''')
-        user_command = input("Confused on where to start? Type HELP. Or, type in the command you wish you use. ")  
-#Races Menu
+        Menu_die()
+        Menu_back() 
 elif user_command == "RACES":
-    print(''' Available races:
-        Dwarf
-        Elf
-        Halfling
-        Human
-        Dragonborn
-        Gnomes
-        Half-Elf
-        Half-Orc
-        Tiefling''')
-    user_command = input("Please type in the command you wish you use. ")
-    if user_command == "CLASSES":
-        print(''' Available classes:''')
-        user_command = input("Please type in the command you wish you use. ")        
-    elif user_command == "BACK":
-        print('''Welcome to the D&D Character Creator! This program can help beginners
-        make their first D&D character at first level!''')
-        user_command = input("Confused on where to start? Type HELP. Or, type in the command you wish you use. ") 
-    elif user_command == "DIE":
-        print('''Die:
-        1d4- a four sided dice using numbers 1-4.
-        1d6- a six sided dice using numbers 1-6.
-        1d8- an eight sided dice using numbers 1-8.
-        1d12- a twelve sided dice using numbers 1-12.
-        1d20- a twenty sided dice using numbers 1-20.''') 
-#Back Menu
-elif user_command == "BACK":
-    print('''Welcome to the D&D Character Creator! This program can help beginners
-    make their first D&D character at first level!''')
-    user_command = input("Confused on where to start? Type HELP. Or, type in the command you wish you use. ") 
-    if user_command == "HELP":
-        print(''' Commands:
-        HELP: shows the list of commands users can use.
-        CLASSES: shows the list of classes available to the player.
-        DIE: shows the types of die available for use.
-        RACES: shows the available races and their attributes.
-        BACK: returns to the start screen.''')
-        user_command = input("Please type in the command you wish you use. ")        
-    elif user_command == "RACES":
-        print(''' Available races:
-        Dwarf
-        Elf
-        Halfling
-        Human
-        Dragonborn
-        Gnomes
-        Half-Elf
-        Half-Orc
-        Tiefling''')
-        user_command = input("Please type in the command you wish you use. ")
-    elif user_command == "CLASSES":
-        print(''' Available classes:''')
-        user_command = input("Please type in the command you wish you use. ")        
-    elif user_command == "DIE":
-        print('''Die:
-        1d4- a four sided dice using numbers 1-4.
-        1d6- a six sided dice using numbers 1-6.
-        1d8- an eight sided dice using numbers 1-8.
-        1d12- a twelve sided dice using numbers 1-12.
-        1d20- a twenty sided dice using numbers 1-20.''')
-
-#Help Menu
-if user_command == "HELP":
-    print(''' Commands:
-    HELP: shows the list of commands users can use.
-    CLASSES: shows the list of classes available to the player.
-    DIE: shows the types of die available for use.
-    RACES: shows the available races and their attributes.
-    BACK: returns to the start screen.
-    PLAY: begins the creation process''')
-    user_command = input("Please type in the command you wish you use. ")
-    if user_command == "CLASSES":
-        print(''' Available classes:''')
-        user_command = input("Please type in the command you wish you use. ")
-    elif user_command == "DIE":
-        print('''Die:
-        1d4- a four sided dice using numbers 1-4.
-        1d6- a six sided dice using numbers 1-6.
-        1d8- an eight sided dice using numbers 1-8.
-        1d12- a twelve sided dice using numbers 1-12.
-        1d20- a twenty sided dice using numbers 1-20.''')
-        user_command = input("Please type in the command you wish you use. ")
-    elif user_command == "RACES":
-        print(''' Available races:
-        Dwarf
-        Elf
-        Halfling
-        Human
-        Dragonborn
-        Gnomes
-        Half-Elf
-        Half-Orc
-        Tiefling''')
-        user_command = input("Please type in the command you wish you use. ")
-    elif user_command == "BACK":
-        print('''Welcome to the D&D Character Creator! This program can help beginners
-        make their first D&D character at first level!''')
-        user_command = input("Confused on where to start? Type HELP. Or, type in the command you wish you use. ")    
-#Classes Menu
-elif user_command == "CLASSES":
-    print(''' Available classes:''')
-    user_command = input("Please type in the command you wish you use. ")
-    if user_command == "DIE":
-        print('''Die:
-        1d4- a four sided dice using numbers 1-4.
-        1d6- a six sided dice using numbers 1-6.
-        1d8- an eight sided dice using numbers 1-8.
-        1d12- a twelve sided dice using numbers 1-12.
-        1d20- a twenty sided dice using numbers 1-20.''')
-        user_command = input("Please type in the command you wish you use. ")
-    elif user_command == "RACES":
-        print(''' Available races:
-        Dwarf
-        Elf
-        Halfling
-        Human
-        Dragonborn
-        Gnomes
-        Half-Elf
-        Half-Orc
-        Tiefling''')
-        user_command = input("Please type in the command you wish you use. ")
-    elif user_command == "BACK":
-        print('''Welcome to the D&D Character Creator! This program can help beginners
-        make their first D&D character at first level!''')
-        user_command = input("Confused on where to start? Type HELP. Or, type in the command you wish you use. ")  
-#Die Menu
-elif user_command == "DIE":
-    print('''Die:
-    1d4- a four sided dice using numbers 1-4.
-    1d6- a six sided dice using numbers 1-6.
-    1d8- an eight sided dice using numbers 1-8.
-    1d12- a twelve sided dice using numbers 1-12.
-    1d20- a twenty sided dice using numbers 1-20.''')
-    user_command = input("Please type in the command you wish you use. ")
-    if user_command == "CLASSES":
-        print(''' Available classes:''')
-        user_command = input("Please type in the command you wish you use. ")        
-    elif user_command == "RACES":
-        print(''' Available races:
-        Dwarf
-        Elf
-        Halfling
-        Human
-        Dragonborn
-        Gnomes
-        Half-Elf
-        Half-Orc
-        Tiefling''')
-        user_command = input("Please type in the command you wish you use. ")
-    elif user_command == "BACK":
-        print('''Welcome to the D&D Character Creator! This program can help beginners
-        make their first D&D character at first level!''')
-        user_command = input("Confused on where to start? Type HELP. Or, type in the command you wish you use. ")  
-#Races Menu
-elif user_command == "RACES":
-    print(''' Available races:
-        Dwarf
-        Elf
-        Halfling
-        Human
-        Dragonborn
-        Gnomes
-        Half-Elf
-        Half-Orc
-        Tiefling''')
-    user_command = input("Please type in the command you wish you use. ")
-    if user_command == "CLASSES":
-        print(''' Available classes:''')
-        user_command = input("Please type in the command you wish you use. ")        
-    elif user_command == "BACK":
-        print('''Welcome to the D&D Character Creator! This program can help beginners
-        make their first D&D character at first level!''')
-        user_command = input("Confused on where to start? Type HELP. Or, type in the command you wish you use. ") 
-    elif user_command == "DIE":
-        print('''Die:
-        1d4- a four sided dice using numbers 1-4.
-        1d6- a six sided dice using numbers 1-6.
-        1d8- an eight sided dice using numbers 1-8.
-        1d12- a twelve sided dice using numbers 1-12.
-        1d20- a twenty sided dice using numbers 1-20.''')
-#Back Menu
-elif user_command == "BACK":
-    print('''Welcome to the D&D Character Creator! This program can help beginners
-    make their first D&D character at first level!''')
-    user_command = input("Confused on where to start? Type HELP. Or, type in the command you wish you use. ") 
-    if user_command == "HELP":
-        print(''' Commands:
-        HELP: shows the list of commands users can use.
-        CLASSES: shows the list of classes available to the player.
-        DIE: shows the types of die available for use.
-        RACES: shows the available races and their attributes.
-        BACK: returns to the start screen.''')
-        user_command = input("Please type in the command you wish you use. ")        
-    elif user_command == "RACES":
-        print(''' Available races:
-        Dwarf
-        Elf
-        Halfling
-        Human
-        Dragonborn
-        Gnomes
-        Half-Elf
-        Half-Orc
-        Tiefling''')
-        user_command = input("Please type in the command you wish you use. ")
-    elif user_command == "CLASSES":
-        print(''' Available classes:''')
-        user_command = input("Please type in the command you wish you use. ")        
-    elif user_command == "DIE":
-        print('''Die:
-        1d4- a four sided dice using numbers 1-4.
-        1d6- a six sided dice using numbers 1-6.
-        1d8- an eight sided dice using numbers 1-8.
-        1d12- a twelve sided dice using numbers 1-12.
-        1d20- a twenty sided dice using numbers 1-20.''') 
-#Play Menu
+        Menu_races()
+        Menu_back()
+elif user_command == "HELP":
+        Menu_help()
+        Menu_back()
+        
+#Starts Creator
 elif user_command == "PLAY":
     #Fake loading
     print('''
